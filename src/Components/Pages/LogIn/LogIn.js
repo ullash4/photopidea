cc26c0cdbb650b81486602f78840cc434f1eb654
ullash4 <programmerullash@gmail.com>
2022-04-17 08:22:03 +0600

    log in page updated

--- src/Components/Pages/LogIn/LogIn.js
+++ src/Components/Pages/LogIn/LogIn.js
@@ -5,25 +5,42 @@ import { Link } from "react-router-dom";
 const LogIn = () => {
   return (
     <>
-      <div className="w-50 mx-auto my-5  p-5 rounded-3 shadow-lg">
-        <Form>
-          <h1 className="text-center mb-3 ">Log In</h1>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <p>New to PhotoPedia ? <Link to='register'>Register here</Link> </p>
-          <Button variant="primary" type="submit">
-            Log In
-          </Button>
-        </Form>
+      <div className="container my-5 p-5 shadow-lg rounded-3">
+        <div className="row g-5">
+          <div className="col-12 col-md-5">
+            <img
+              className="img-fluid rounded-3 "
+              src="https://media.giphy.com/media/ZgTR3UQ9XAWDvqy9jv/giphy.gif"
+              alt=""
+            />
+          </div>
+          <div className="col-12 col-md-7">
+            <div className=" p-4">
+            <Form>
+              <h1 className="text-center mb-3 ">Log In</h1>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <p>
+                New to PhotoPedia ?{" "}
+                <Link className="text-decoration-none" to="/register">
+                  Register here
+                </Link>{" "}
+              </p>
+              
+              <Button variant="primary" type="submit">
+                Log In
+              </Button>
+              <button className="btn btn-link text-decoration-none ms-3">Forgot password ?</button>
+            </Form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
