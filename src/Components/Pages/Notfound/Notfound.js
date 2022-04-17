c924577c2ffefb563d8b8633b680ade613452078
ullash4 <programmerullash@gmail.com>
2022-04-17 12:20:40 +0600

    404 page created

--- src/Components/Pages/Notfound/Notfound.js
+++ src/Components/Pages/Notfound/Notfound.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Notfound = () => {
     return (
-        <div>
-            <h1>not found page</h1>
+        <div className='my-2 p-5'>
+            <div className='d-flex flex-column flex-md-row justify-content-evenly align-items-center'>
+                <div className=''>
+                    <img className='' src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif" alt="" />
+                </div>
+                <div className=''>
+                    <h1 className='display-2 fw-bolder'>You are Lost Buddy</h1>
+                    <p className='display-5'>The page you have searching for is not found. Please click below button to return home</p>
+                    <Link to={'/home'}> <button className='px-5 py-3 bg-dark rounded-3 text-white fs-4 border-0'>Return Home</button></Link>
+                </div>
+            </div>
         </div>
     );
 };
