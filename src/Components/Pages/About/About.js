fbe9f26b7bf3c7d5aa371fdac289091572acf5bf
ullash4 <programmerullash@gmail.com>
2022-04-18 14:45:33 +0600

    readme file added

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -6,10 +6,11 @@ const About = () => {
             <div className='d-flex justify-content-evenly align-items-center gap-3'>
                 <div>
                     <h1>Something about me</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit inventore nihil ab qui quis quas, hic nam optio repellendus quae voluptate natus iusto fugiat vel ut quia a, minima fuga aperiam dolor, placeat beatae excepturi aliquid. Accusamus amet odio maxime? Vitae beatae, unde cum ea sequi odio nulla ad optio.</p>
+                    <p>Hello there, This is Mark William. A pasionate photographer. My bigest ahicvemnet is to work with Commercial Clients include ColorPlus,Linen Club, Levis, Nike, Saffola, Flipkart, Myntra, Biba, Prestige Man Store, Jabong, Peter England, Tata Sky, Wildcraft, Being Human, among others. And my next target is to work with Grazia, Vogue,Harpers Bazaar, GQ, Man's World,Verve, Femina, Filmfare, People Magazine, Marie Claire</p>
+                    <p>For contact Email at : programmer@gmail.com</p>
                 </div>
                 <div>
-                <img className='rounded-3 shadow-sm' src="https://i.ibb.co/vLnRnD2/Untitled-design-1.png" alt="" />
+                <img className='rounded-3 shadow-sm' src="https://i.ibb.co/8rNFSqY/Untitled-design-11.png" alt="" />
                 </div>
             </div>
         </div>
