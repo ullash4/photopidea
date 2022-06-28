cf8ca8cf007bfa957a0f389551e438ebd75d03ac
ullash4 <programmerullash@gmail.com>
2022-06-28 18:18:36 +0600

    contact us page created

--- src/Components/Pages/ContactUs/ContactUs.js
+++ src/Components/Pages/ContactUs/ContactUs.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
+import GetInTouch from "./GetInTouch";
 
 function ContactUs() {
   return (
-    <div>
-        <div className=''>
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, eaque!</div>
-            <img src="https://i.ibb.co/92GcYp8/Untitled-design-4.png" alt="" />
+    <div className="">
+      <div className="container my-5 d-flex flex-column flex-md-row justify-content-evenly align-items-center gap-3">
+        <div>
+          <h1>Have a project in mind</h1>
+          <p>
+            Reach out to us We can make something awsome together
+          </p>
+          
         </div>
-        <div></div>
+        <div>
+        <img className='img-fluid rounded-3' src="https://i.ibb.co/yptZMv7/Untitled-design-9.png" alt="" />
+        </div>
+      </div>
+      <GetInTouch />
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
---
+++ src/Components/Pages/ContactUs/GetInTouch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function GetInTouch() {
+  return (
+    <div className=' bg-light p-5 '>
+        <div className='w-75 mx-auto my-5 p-5 bg-white shadow-lg rounded-3'>
+        <h4  className='text-center'
+        >Get in Touch</h4>
+        <div className='row g-5'>
+            <div>
+
+            </div>
+            <div></div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default GetInTouch
